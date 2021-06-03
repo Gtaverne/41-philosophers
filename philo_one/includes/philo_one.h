602d4a7a4a1598ec4f0b253e28c7eb89f068d1ea
Georges TAVERNE <gtaverne@e3r3p2.42.fr>
2021-06-03 19:55:54 +0200

    First push

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -1,10 +1,22 @@
-#include <unistd.h>
-#include <stdio.h>
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
 
 typedef struct s_a {
-	int	n_of_philo;
-	int t_to_die;
-	int	t_to_eat;
-	int	t_to_sleep;
-	int	n_meals; 
+	struct timeval	base_t;
+	int				n_of_philo;
+	int				t_to_die;
+	int				t_to_eat;
+	int				t_to_sleep;
+	int				n_meals; 
 } t_a;
+
+//utils
+int		ft_atoi(const char *nptr, t_a *a);
+void	ft_cleanexit(t_a *a, char *str);
+
+#endif
--- philo_one/srcs/utils.c
+++ philo_one/srcs/utils.c
@@ -3,27 +3,26 @@
 void	ft_cleanexit(t_a *a, char *str)
 {
 	printf("Exit : %s\n", str);
+	a->n_of_philo = 0;
 	exit(0);
 }
 
 int	ft_atoi(const char *nptr, t_a *a)
 {
-	long long int		i;
-	long long int		r;
+	int		i;
+	int		r;
 
 	i = 0;
 	r = 0;
-	while (nptr[i] != '\0')
+	while (nptr && nptr[i])
 	{
 		if (nptr[i] >= '0' && nptr[i] <= '9')
 			r = r * 10 + nptr[i] - '0';
-		else if (nptr[i] == 0)
-			break ;
 		else
-			ft_cleanexit(a, "Non numerical argument");
+			ft_cleanexit(a, "Arguments non numeriques");
 		i++;
 	}
-	if (r >= 36000000)
-		ft_cleanexit(a, "Valeur enorme, on va attendre mille ans...");
+	if (r >= 3600000)
+		ft_cleanexit(a, "Valeur enorme, on va attendre des heures...");
 	return (r);
 }
